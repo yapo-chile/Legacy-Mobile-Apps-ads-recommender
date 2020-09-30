1ecf17b03abdf5d6ca5ce02d1ef2e1508eefa71c
Gustavo Saavedra <gustavo.saavedra@adevinta.com>
2020-09-30 09:27:45 -0300

    feat/pro-suggestions-utils - Add elastic and fileTools infrastructure

---
+++ pkg/infrastructure/elasticsearch.go
@@ -0,0 +1,231 @@
+package infrastructure
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"github.mpi-internal.com/Yapo/pro-carousel/pkg/interfaces/loggers"
+)
+
+type ElasticItem struct {
+	ID   string
+	Data interface{}
+}
+type ElasticHandler struct {
+	client        *elasticsearch.Client
+	batchSize     int
+	searchTimeout time.Duration
+	logger        loggers.Logger
+}
+type BulkResponse struct {
+	Errors bool       `json:"errors"`
+	Items  []ItemBulk `json:"items"`
+}
+type ItemBulk struct {
+	Index IndexBulk `json:"index"`
+}
+type IndexBulk struct {
+	ID     string    `json:"_id"`
+	Result string    `json:"result"`
+	Status int       `json:"status"`
+	Error  ErrorBulk `json:"error"`
+}
+type ErrorBulk struct {
+	Type   string    `json:"type"`
+	Reason string    `json:"reason"`
+	Cause  CauseBulk `json:"caused_by"`
+}
+type CauseBulk struct {
+	Type   string `json:"type"`
+	Reason string `json:"reason"`
+}
+
+// NewElasticHandlerHandler will create a new instance of a custom http request handler
+func NewElasticHandlerHandler(
+	maxIdleConns, maxIdleConnsPerHost, MaxConnsPerHost, IdleConnTimeout, batchSize int, searchTimeout time.Duration,
+	addresses string,
+	logger loggers.Logger) *ElasticHandler {
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			addresses,
+		},
+		Transport: &http.Transport{
+			MaxIdleConns:        maxIdleConns,
+			MaxIdleConnsPerHost: maxIdleConnsPerHost,
+			MaxConnsPerHost:     MaxConnsPerHost,
+			IdleConnTimeout:     time.Duration(IdleConnTimeout) * time.Second,
+		},
+	}
+	es7, _ := elasticsearch.NewClient(cfg)
+	return &ElasticHandler{
+		client:        es7,
+		batchSize:     batchSize,
+		searchTimeout: searchTimeout,
+		logger:        logger,
+	}
+}
+
+// Info gets elastic cluster info
+func (es *ElasticHandler) Info() (interface{}, error) {
+	res, err := es.client.Info()
+	if err != nil {
+		return nil, err
+	}
+	res.String() // https://github.com/elastic/go-elasticsearch/issues/123
+	defer res.Body.Close()
+	// Check response status
+	if res.IsError() {
+		err = fmt.Errorf("Error: ", res.String())
+	}
+	return res, err
+}
+
+// Create generates a new elastic index
+func (es *ElasticHandler) Create(index string) error {
+	if _, err := es.client.Indices.Delete([]string{index}); err != nil {
+		return fmt.Errorf("cannot delete index: %s", err)
+	}
+	res, err := es.client.Indices.Create(index)
+	res.String() // https://github.com/elastic/go-elasticsearch/issues/123
+	if err != nil || res.IsError() {
+		return fmt.Errorf("Cannot create index: %s", err)
+	}
+	return nil
+}
+
+// PutMapping put mapping on a index on elastic search
+// mapping byte slice with the poperties json data
+// index string with index name
+func (es *ElasticHandler) PutMapping(mapping []byte, index string) error {
+	res, err := es.client.Indices.PutMapping(
+		[]string{index},
+		strings.NewReader(string(mapping)),
+	)
+	res.String() // https://github.com/elastic/go-elasticsearch/issues/123
+	if err != nil || res.IsError() {
+		return fmt.Errorf("Cannot put mapping on index: %s", err)
+	}
+	return nil
+}
+
+// Search gets response from elastic using query
+// index string with index name
+// query string with query string
+// size how many hits are returned
+// from in which item the search process begins
+func (es *ElasticHandler) Search(index, query string, size, from int) (string, error) {
+	if size <= 0 {
+		size = 10
+	}
+	res, err := es.client.Search(
+		es.client.Search.WithIndex(index),
+		es.client.Search.WithBody(strings.NewReader(query)),
+		es.client.Search.WithSize(size),
+		es.client.Search.WithFrom(from),
+		es.client.Search.WithTimeout(es.searchTimeout),
+		es.client.Search.WithPretty(),
+	)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	// Check response status
+	response, err := ioutil.ReadAll(res.Body)
+	return string(response), err
+}
+
+// Bulk insert a data collection in elastic
+// collection items to be send
+// index string with index name
+// action string to indicate which action should be done, ex: index, update, ...
+func (es *ElasticHandler) Bulk(collection []ElasticItem, index, action string) error {
+	var blk *BulkResponse
+	var buf bytes.Buffer
+	var res *esapi.Response
+	var raw map[string]interface{}
+	numErrors, numIndexed, numItems, numBatches, currBatch := 0, 0, 0, 0, 0
+	count := len(collection)
+	if count%es.batchSize == 0 {
+		numBatches = (count / es.batchSize)
+	} else {
+		numBatches = (count / es.batchSize) + 1
+	}
+	// Loop over the collection
+	for i, item := range collection {
+		numItems++
+		currBatch = i / es.batchSize
+		if i == count-1 {
+			currBatch++
+		}
+		// Prepare the metadata payload
+		meta := []byte(fmt.Sprintf(`{ "%s" : { "_id" : "%d" } }%s`, action, item.ID, "\n"))
+		// Prepare the data payload: encode to JSON
+		data, err := json.Marshal(item.Data)
+		if err != nil {
+			es.logger.Error("Cannot encode item %d: %s", item.ID, err)
+			continue
+		}
+		// Append newline to the data payload
+		data = append(data, "\n"...)
+		// Append payloads to the buffer (ignoring write errors)
+		buf.Grow(len(meta) + len(data))
+		buf.Write(meta)
+		buf.Write(data)
+		if i > 0 && i%es.batchSize == 0 || i == count-1 {
+			es.logger.Error("Bulk [%d de %d] ", currBatch, numBatches)
+			// execute bulk
+			res, err = es.client.Bulk(bytes.NewReader(buf.Bytes()), es.client.Bulk.WithIndex(index))
+			if err != nil {
+				es.logger.Error("ElasticSearch Bulk Fails: indexing batch %d: %s", currBatch, err)
+			}
+			// If the whole request failed, print error and mark all documents as failed
+			res.String()
+			if res.IsError() {
+				numErrors += numItems
+				if err := json.NewDecoder(res.Body).Decode(&raw); err != nil {
+					es.logger.Error("Failure to to parse response body: %s", err)
+				} else {
+					es.logger.Error("  Error: [%d] %s: %s",
+						res.StatusCode,
+						raw["error"].(map[string]interface{})["type"],
+						raw["error"].(map[string]interface{})["reason"],
+					)
+				}
+			} else {
+				if err := json.NewDecoder(res.Body).Decode(&blk); err != nil {
+					es.logger.Error("Failure to to parse response body: %s", err)
+				} else {
+					for _, d := range blk.Items {
+						// ... so for any HTTP status above 201 ...
+						if d.Index.Status > 201 {
+							// ... increment the error counter ...
+							numErrors++
+							// ... and print the response status and error information ...
+							es.logger.Error("  Error: [%d]: %s: %s: %s: %s",
+								d.Index.Status,
+								d.Index.Error.Type,
+								d.Index.Error.Reason,
+								d.Index.Error.Cause.Type,
+								d.Index.Error.Cause.Reason,
+							)
+						} else {
+							// ... otherwise increase the success counter.
+							numIndexed++
+						}
+					}
+				}
+			}
+			res.Body.Close()
+			buf.Reset()
+			numItems = 0
+		}
+	}
+	return nil
+}
---
+++ pkg/infrastructure/fileTools.go
@@ -0,0 +1,96 @@
+package infrastructure
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+// FileTools struct to interact with a directory files
+type FileTools struct {
+	FilesPath string
+	Extension string
+}
+
+// NewFileTools will create a new instance of a custom File Tool handler
+func NewFileTools(filePath string, extension string) *FileTools {
+	return &FileTools{
+		FilesPath: filePath,
+		Extension: extension,
+	}
+}
+
+// FileExist checks if a file exists and is not a directory before we
+// try using it to prevent further errors.
+func (t *FileTools) FileExist(fileName string) bool {
+	info, err := os.Stat(t.FilesPath + fileName)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// LoadJSONFromFile Loads a Json from file
+func (t *FileTools) LoadJSONFromFile(fileName string, dest interface{}) error {
+	content, err := ioutil.ReadFile(t.FilesPath + fileName)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(content, dest)
+}
+
+// ListFilesFromPath gets a list of files in a directory
+func (t *FileTools) ListFilesFromPath() (map[string]string, error) {
+	files := make(map[string]string)
+	err := filepath.Walk(t.FilesPath, func(path string, info os.FileInfo, err error) error {
+		fmt.Printf("\n file: %+v", info.Name())
+
+		if !info.IsDir() && filepath.Ext(path) == t.Extension {
+			files[strings.TrimSuffix(info.Name(), t.Extension)] = info.Name()
+		}
+		return nil
+	})
+	return files, err
+}
+
+// LoadTemplatesFromFolder loads templates from
+// an specific folder
+func (t *FileTools) LoadTemplatesFromFolder() map[string]*template.Template {
+	fmt.Printf("\nt : %+v", t)
+	fmt.Printf("\npath : %+v", t.FilesPath)
+	templatesToLoad, _ := t.ListFilesFromPath()
+	templates := make(map[string]*template.Template)
+	for key, file := range templatesToLoad {
+		rawTemplate, err := template.ParseFiles(t.FilesPath + file)
+		if err == nil {
+			templates[getKey(key)] = rawTemplate
+		}
+	}
+	return templates
+}
+
+// LoadTemplatesFromFiles loads templates from
+// multiple files of a folder
+func (t *FileTools) LoadTemplatesFromFiles(templatesToLoad map[string]string) map[string]*template.Template {
+	templates := make(map[string]*template.Template)
+	for key, file := range templatesToLoad {
+		rawTemplate, err := template.ParseFiles(t.FilesPath + file)
+		if err == nil {
+			templates[getKey(key)] = rawTemplate
+		}
+	}
+	return templates
+}
+
+// LoadTemplateFromFile loads a single template
+func (t *FileTools) LoadTemplateFromFile(templateName string) (*template.Template, error) {
+	return template.ParseFiles(t.FilesPath + templateName)
+}
+
+func getKey(key string) string {
+	return strings.Split(key, ".")[0]
+}
