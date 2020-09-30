2c3099f7b18bafbcd2deb34e439d3788f54a46b7
Gustavo Saavedra <gustavo.saavedra@adevinta.com>
2020-09-30 18:41:05 -0300

    feat/pro-suggestions - Add get suggestions usecase

---
+++ pkg/usecases/usecases.go
@@ -0,0 +1,11 @@
+package usecases
+
+import (
+	"github.mpi-internal.com/Yapo/pro-carousel/pkg/domain"
+)
+
+// GetSuggestionsInteractor defines the available methods for this interactor
+type GetSuggestionsInteractor interface {
+	// GetProSuggestions will get all suggestions for the given listID
+	GetProSuggestions(ListID string, size, from int) (ads []domain.Ad, err error)
+}
